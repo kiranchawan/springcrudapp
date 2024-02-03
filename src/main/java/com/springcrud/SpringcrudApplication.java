047af8fca5e2ce871ac7fada06010ec53e0ef0d1
kiran chavan <kchawan206@gmail.com>
2024-02-03 19:40:20 +0530

    added variable name mahesh main in main class

--- src/main/java/com/springcrud/SpringcrudApplication.java
+++ src/main/java/com/springcrud/SpringcrudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringcrudApplication {
-
+    private static final String = "mahesh main";
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcrudApplication.class, args);
 	}
